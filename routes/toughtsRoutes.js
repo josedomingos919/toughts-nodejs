4d1045fc0938cdcfea99ea783913e4a6773ae78a
josedomingos919 <josedomingos919@gmail.com>
2023-01-08 23:44:46 +0100

    feat: setting up routes

---
+++ routes/toughtsRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { ToughtController } = require("../controllers/ToughtController");
+
+router.get("/", ToughtController.showToughts);
+
+module.exports = { toughtRoutes: router };
